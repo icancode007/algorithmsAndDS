f14ef41788bbde0f479c84f0df40258d0b577863
Jose Franny Rodriguez <icancode911@gmail.com>
2019-09-26 00:51:45 -0400

    reverse an integer

--- Js/palindromes.js
+++ Js/palindromes.js
@@ -5,14 +5,23 @@
 // and punctuation in determining if the string is a palindrome.
 // --- Examples:
 //   palindrome("abba") === true
-
 //   palindrome("abcdefg") === false
 
-// solution 1
-// const palindrome = str => str.split('').reverse().join( '' )  === str;
-
+/*
+    //solution 1
+        have to use a splitter
+        for each of str chars,
+            and then iterate againg to reverse an joing increasing the number of work and times to 3 * n
+        but in Big O so small numbers are trivial to a large set of data, so we can say its O( n ) as well
+    const palindrome = str => {
+         if ( !!str ) console.log( "You will need to provide an actual string, and it should at least have a lenght of 1" );
+        str.split('').reverse().join( '' )  === str;
+    }
+*/
 // solution 2
 function palindrome(str) {
+     if (!!str) console.log( "You will need to provide an actual string, and it should at least have a lenght of 1" );
+
     const lastIdx = str.length - 1;
     let front = 0;
     let end = lastIdx;
@@ -26,7 +35,10 @@ function palindrome(str) {
     return false;
 }
 
+// 2 - Explanation: It present a Big O of o( n ) since the amount of iterations is lineal and will not exceed the input size
+
 // solution 3
 // const palindrome = str => str.split('').every((char, idx) => char === str[str.length -1 - idx]);
 
+// 3 - Explanation: just like solution one this one depends on extra iterations, but in this case its only 2 so o(2n) which still is n
 module.exports = palindrome;
--- Js/reverseString.js
+++ Js/reverseString.js
@@ -6,41 +6,60 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-//solution 1
-// function reverse(str) {
-//     let reverser = "";
-//     for(let i = str.length-1; i > -1; i--) {
-//         reverser += str[i];
-//     }
-//     return reverser;
-// }
+/*
+//solution 1 
+function reverse(str) {
+    if(!!str) console.log("You will need to provide an actual string, and it should at least have a lenght of 1");
+
+    let reverser = "";
+    for(let i = str.length-1; i > -1; i--) {
+        reverser += str[i];
+    }
+    return reverser;
+}
 
 //solution 2
-// function reverse( str ) {
-//     let iterator = str[ Symbol.iterator ]();
-//     let curr = iterator.next();
-//     const chars = [];
-//     chars.unshift( curr.value );
-//     while ( !curr.done ) {
-//         curr = iterator.next();
-//         chars.unshift(curr.value)
-//     }
-//     return chars.join('');
-// }
+function reverse( str ) {
+    if ( !!str ) console.log( "You will need to provide an actual string, and it should at least have a lenght of 1" );
+    let iterator = str[ Symbol.iterator ]();
+    let curr = iterator.next();
+    const chars = [];
+    chars.unshift( curr.value );
+    while (!curr.done) {
+        curr = iterator.next();
+        chars.unshift(curr.value)
+    }
+    return chars.join('');
+}
 
-//solution 3 - Completely cheating :P
-// const reverse = str => str.split('').reverse().join('');
+//solution 3
+function reverse( str ) {
+    if ( !!str ) console.log( "You will need to provide an actual string, and it should at least have a lenght of 1" );
+    let reversed = '';
+    for ( let char of str ) {
+        reversed = char + reversed;
+    }
+    return reversed;
+}
+        1 - 3 Explenation: 
+            All the above solutions present a Big O of o(n) 
+            since the amount of iterations is lineal and will not exceed the input size
 
-//solution 4 
-// function reverse(str) {
-//     let reversed = '';
-//     for(let char of str) {
-//         reversed = char + reversed;
-//     }
-//     return reversed;
-// }
+*/
 
+/*
+        4 - 5 Explenation:
+            Both last solutions have to use a splitter for each of str chars, 
+            and then iterate againg to reverse an joing increasing the number of work and times to 3 * n 
+            but in Big O so small numbers are trivial to a large set of data, so we can say its O(n) as well
+//solution 4 - Completely cheating :P
+const reverse = str => {
+    if ( !!str ) console.log( "You will need to provide an actual string, and it should at least have a lenght of 1" );
+    str.split('').reverse().join('');
+}
+*/
 //solution 5
 function reverse(str) {
     return str.split.reduce((reverser, currArrElement) => reverser += currArrElement, '')
 }
+
