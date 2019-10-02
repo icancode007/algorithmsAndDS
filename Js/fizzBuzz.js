f17dcb1b8df92585ede3dc5e47b63c1d30270efe
Jose Franny Rodriguez <icancode911@gmail.com>
2019-10-02 00:22:23 -0400

    fizz-buzz-2

--- Js/fizzBuzz.js
+++ Js/fizzBuzz.js
@@ -14,13 +14,40 @@
 
 function fizzBuzz( n ) {
     for ( let i = 1; i <= n; i++ ) {
-        if ( i % 3 === 0 && i % 5 === 0 ) {} else if ( i % 3 === 0 ) {
+        if ( i % 3 === 0 && i % 5 === 0 ) {
+            console.log('fizzbuzz')
+        } else if ( i % 3 === 0 ) {
             console.log( 'fizz' );
         } else if ( i % 5 === 0 ) {
             console.log( 'buzz' );
         } else {
             console.log( i );
         }
+    }
+}
+
+function fizzBuzz(n){
+    const divisibleBy3 = counter % 3 == 0;
+    const divisibleBy5 = counter % 5 == 0;
+    let counter = 1;
+
+    while(counter <= n) {
+        if(divisibleBy3 && divisibleBy5) {
+            console.log('fizzbuzz');
+        }
+        
+        if(divisibleBy3 && !divisibleBy5) {
+            console.log('fizz');
+        }
+        
+        if(divisibleBy5 && !divisibleBy3) {
+            console.log('buzz');
+        }
+        
+        if (!divisibleBy3 && !divisibleBy5 ) {
+            console.log(counter)
+        }
+        counter++;
     }
 }
 module.exports = fizzBuzz;
